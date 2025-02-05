8e42ad1fceb3483baf670fdade1357b44af789bb
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-02-05 14:02:58 +0300

    updated script.js with classes and Attributes

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////
 //////////////////////////////
 
+/*
 //Selecting Elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -64,3 +65,46 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click',function() {
   message.remove();
 })
+
+// styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%'
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company','Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('herf'));
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
+//classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+*/
+
